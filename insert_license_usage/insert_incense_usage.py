8ecfceb29b106f3bf97b7c429a0df3020745b045
심선우 <suns1997@gmail.com>
2025-02-04 17:02:41 +0900

    초기 insert_license_usage.py 추가

---
+++ insert_license_usage/insert_incense_usage.py
@@ -0,0 +1,25 @@
+import os
+import sys
+import sqlalchemy as sa
+
+from enum import Enum
+from datetime import date
+
+from alembic.config import Config
+from alembic import command
+
+from smp_backend.app.core.config import dconf
+
+import sqlalchemy as sa
+from sqlalchemy.orm import sessionmaker
+from smp_backend.app.models import user, usergroup, relation, company, admin
+
+original_db_type = dconf.DB_TYPE
+dconf.DB_TYPE = "postgresql"
+
+engine = sa.create_engine(dconf.DB_URL, echo=True)
+
+Session = sessionmaker(bind=engine)
+session = Session()
+
+sa.select(User)
